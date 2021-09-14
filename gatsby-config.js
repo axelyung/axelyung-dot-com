34eccfcf7ab05ca3786c615a9ba3103a368fb2b4
Axel Yung <axelyung@gmail.com>
2021-09-14 14:32:53 -0700

    eslint gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,41 @@
 const plugins = [
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-image',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sitemap',
-    {
-	resolve: 'gatsby-plugin-manifest',
-	options: {
+  'gatsby-plugin-styled-components',
+  'gatsby-plugin-image',
+  'gatsby-plugin-react-helmet',
+  'gatsby-plugin-sitemap',
+  {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
 	    icon: 'src/images/icon.png',
-	},
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-	resolve: 'gatsby-source-filesystem',
-	options: {
+  },
+  'gatsby-plugin-sharp',
+  'gatsby-transformer-sharp',
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
 	    name: 'images',
 	    path: './src/images/',
-	},
-	__key: 'images',
     },
+    __key: 'images',
+  },
 ];
 
-const {GANALYTICS_TRACKING_ID = ''} = process.env;
+const { GANALYTICS_TRACKING_ID = '' } = process.env;
 if (GANALYTICS_TRACKING_ID) {
-    console.log({GANALYTICS_TRACKING_ID});
-    plugins.push({
-	resolve: 'gatsby-plugin-google-analytics',
-	options: {
+  console.log({ GANALYTICS_TRACKING_ID });
+  plugins.push({
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
 	    trackingId: GANALYTICS_TRACKING_ID,
-	},
-    });
+    },
+  });
 }
 
 module.exports = {
-    siteMetadata: {
-	siteUrl: 'https://axelyung.com',
-	title: 'Axel Yung',
-    },
-    plugins,
+  siteMetadata: {
+    siteUrl: 'https://axelyung.com',
+    title: 'Axel Yung',
+  },
+  plugins,
 };
