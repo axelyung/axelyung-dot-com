a9104bf176a2982997cd1ae3c854ca95259869aa
Axel Yung <axelyung@gmail.com>
2021-09-14 21:07:44 -0700

    add typescript to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,17 @@
 module.exports = {
+  parser: `@typescript-eslint/parser`,
+  plugins: [`@typescript-eslint`, `prettier`],
   extends: [
-    "eslint:recommended",
-    "airbnb",
-    "airbnb/hooks",
-    "plugin:prettier/recommended",
+    `eslint:recommended`,
+    `airbnb`,
+    `airbnb/hooks`,
+    `plugin:prettier/recommended`,
   ],
-  plugins: ["prettier"],
   rules: {
-    indent: [2, 2],
+    quotes: [2, `backtick`],
+    'prettier/prettier': [2, { singleQuote: true }],
+    'react/jsx-filename-extension': [1, { extensions: [`.tsx`] }],
+    'no-use-before-define': 0,
+    '@typescript-eslint/no-use-before-define': 2,
   },
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 
 // styles
 const pageStyles = {
-  color: '#232129',
+  color: `#232129`,
   padding: 96,
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
+  fontFamily: `-apple-system, Roboto, sans-serif, serif`,
 };
+
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
@@ -16,9 +17,7 @@ const headingStyles = {
 const IndexPage = () => (
   <main style={pageStyles}>
     <title>Home Page</title>
-    <h1 style={headingStyles}>
-      Welcome to axelyung.com
-    </h1>
+    <h1 style={headingStyles}>Welcome to axelyung.com</h1>
   </main>
 );
 
